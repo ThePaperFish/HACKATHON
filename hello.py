3e39efb8343649e804b33d2ca532380371d01354
levin98 <30562200+levin98@users.noreply.github.com>
2017-07-30 10:46:02 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-def banner(type):
+def banner():
     print('-----' *18)
     print('\t' *3, "South City Plaza Apartment")
     print('-----' *18)
 
 banner()
+number_a = int(input())
+number_b = int(input())
