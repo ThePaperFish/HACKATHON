02816565849e3d299052e5118ea8826e9d902090
ThePaperFish <lego_0603@hotmail.com>
2017-07-30 10:36:53 +0800

    SCP
    baner only

--- hello.py
+++ hello.py
@@ -1,22 +1,6 @@
-def small(numa,numb):
-    dif = abs(numa - numb)
-    sumab = numa + numb
-    return((sumab -dif)/2)
+def banner():
+    print('-----' *18)
+    print('\t' *3, "South City Plaza Apartment")
+    print('-----' *18)
 
-def smallest(amount):
-    if (amount == 0):
-        return num[0]
-    else:
-        return small(smallest(amount -1),num[amount])
-
-num = []
-temp1 = 0
-temp = input("Enter Number " + str(len(num) +1) + " : ")
-
-while (temp.isdigit()):
-    num.append(temp)
-    temp = input("Enter Number " + str(len(num) +1) + " : ")
-
-num = [int(s) for s in num]
-
-print(smallest(len(num)-1))
+banner()
