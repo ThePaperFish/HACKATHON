9f960000b0bdfdd69155832b4ca886e41826849b
ThePaperFish <lego_0603@hotmail.com>
2017-07-30 11:18:14 +0800

    Banner and user

--- hello.py
+++ hello.py
@@ -4,26 +4,27 @@ def banner(Type):
         print('-----' *18)
         print('\t' *3, "South City Plaza Apartment")
         print('-----' *18)
+        print('\n')
+        
+    if Type == 1:
+        a = """ Apartment Type A
+               - 2 bedrooms and equiped with kitchen and laundry
+               - Monthly rental is RM300 per room """
+        b = """ Apartment Type B
+               - 3 bedrooms including one master bedroom with attached bedroom
+               - No kitchen and laundry facilities
+               - Monthly rental is RM200 with normal room and additional 40% for master room """
+        print(a, b, sep='\n')
+        print('\n')
 
-banner(0) # 0 is start
-
-def banner():
-    print('-----' *18)
-    print('\t' *3, "South City Plaza Apartment")
-    print('-----' *18)
-    
-def type_banner():
-    a = """ Apartment Type A
-           - 2 bedrooms and equiped with kitchen and laundry
-           - Monthly rental is RM300 per room """
-    b = """ Apartment Type B
-           - 3 bedrooms including one master bedroom with attached bedroom
-           - No kitchen and laundry facilities
-           - Monthly rental is RM200 with normal room and additional 40% for master room """
-    print(a, b, sep='\n')
-
+    if Type == 2:
+        print ("Welcome!")
+        
 banner(0)
-type_banner()
+banner(1)
 
-Username = input()
-Password = input()
+username = input("Please enter your username  : ")
+password = input("Please enter your password  : ")
+
+if username.equals("user1") and password.equals("user1") :
+    banner(2)
